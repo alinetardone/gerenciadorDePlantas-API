ea80f408595f8173dd8e1265ee74d455dd4cf845
AlineTardoneHagens <aline.tardone@hagens.com.br>
2023-10-29 15:46:46 -0300

    ✨ rotas para rega

--- src/app/modules/regas/routes/regas.routes.ts
+++ src/app/modules/regas/routes/regas.routes.ts
@@ -3,12 +3,12 @@ import { Router } from "express";
 import { RegasController } from "../controller/regas.controller";
 
 const regaController = new RegasController();
-const plantasRoutes = Router();
+const regasRoutes = Router();
 
-plantasRoutes.post("/", regaController.create)
-plantasRoutes.put("/:id", regaController.update)
-plantasRoutes.delete("/:id", regaController.delete)
-plantasRoutes.get("/:id", regaController.buscaRega)
-plantasRoutes.get("/planta/:plantaId", regaController.buscaRegaPorPlantaId)
+regasRoutes.post("/", regaController.create)
+regasRoutes.put("/:id", regaController.update)
+regasRoutes.delete("/:id", regaController.delete)
+regasRoutes.get("/:id", regaController.buscaRega)
+regasRoutes.get("/planta/:plantaId", regaController.buscaRegaPorPlantaId)
 
-export { plantasRoutes };
+export { regasRoutes };
--- src/http/routes/index.ts
+++ src/http/routes/index.ts
@@ -2,13 +2,14 @@ import { Request, Response, Router } from "express";
 import { usuariosRoutes } from "../../app/modules/usuarios/routes/usuarios.routes";
 import { plantasRoutes } from "../../app/modules/plantas/routes/plantas.routes";
 import { imagensRoutes } from "../../app/modules/imagensPlantas/routes/imagens.routes";
+import { regasRoutes } from "../../app/modules/regas/routes/regas.routes";
 
 const routes = Router();
 
 routes.use("/usuarios", usuariosRoutes)
 routes.use("/plantas", plantasRoutes)
 routes.use("/imagens", imagensRoutes)
-
+routes.use("/regas", regasRoutes)
 
 routes.get('/', (_req: Request, res: Response) => {
   return res.status(301).redirect('/api-docs');
